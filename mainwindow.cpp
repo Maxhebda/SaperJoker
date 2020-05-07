090510feced687290435cfdd736efd4783689609
Maksymilian Hebda <maksymilian.hebda@gmail.com>
2020-05-07 13:36:48 +0200

    add joker

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,7 +60,9 @@ void MainWindow::showBoard()
                 myButton[y][x]->setIcon(QIcon(":/res/01mine.png"));
             if (saperBoard.get(y,x).isFlag())
                 myButton[y][x]->setIcon(QIcon(":/res/03flag.png"));
-            if ((saperBoard.get(y,x).getStateDown()>=0 && saperBoard.get(y,x).getStateDown()<9)||saperBoard.get(y,x).getStateDown()==10)
+            if (saperBoard.get(y,x).getStateDown()==10)
+                myButton[y][x]->setIcon(QIcon(":/res/04joker.png"));
+            if (saperBoard.get(y,x).getStateDown()>0 && saperBoard.get(y,x).getStateDown()<9)
                 {
                     myButton[y][x]->setText(QVariant(saperBoard.get(y,x).getStateDown()).toString());
                     myButton[y][x]->setStyleSheet("color: "+saperBoard.getColorNumber(y,x)+";");
